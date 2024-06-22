2018078d0dce6de153268bf7fd43f8c5eb742f6b
포리 <dongkyun2331@gmail.com>
2024-06-22 09:26:51 +0000

    login signup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+      <div>
+        <Link href="/login">
+            <button>Go to Login</button>
+        </Link>
+        <Link href="/signup">
+            <button>Go to Signup</button>
+        </Link>
+      </div>
     </div>
   );
 };
